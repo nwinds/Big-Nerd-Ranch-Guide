0ae1be756b171f6e5298c5c21eb47d6d251d0b29
nwinds <namingwinds@gmail.com>
2015-07-14 16:55:00 +0800

    to p144

--- mywork/Hypnosister/BNRHypnosisViewController.m
+++ mywork/Hypnosister/BNRHypnosisViewController.m
@@ -14,7 +14,15 @@
 
 - (void)loadView
 {
-    BNRHypnosisView *backgroundView = [[BNRHypnosisView alloc] init];
+    CGRect frame = [UIScreen mainScreen].bounds;
+    BNRHypnosisView *backgroundView = [[BNRHypnosisView alloc] initWithFrame:frame];
+    
+    CGRect textFieldRect = CGRectMake(40, 70, 240, 30);
+    UITextField *textField = [[UITextField alloc] initWithFrame:textFieldRect];
+    
+    textField.borderStyle = UITextBorderStyleRoundedRect;
+    [backgroundView addSubview:textField];
+    
     self.view = backgroundView;
 }
 
